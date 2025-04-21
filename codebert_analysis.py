2722e189d15d62477b371d9bf62939aeeedcf9bb
mhdfahrurozi <107231096+mhdfahrurozi@users.noreply.github.com>
2025-04-21 22:32:13 +0700

    Create codebert_analysis.py

---
+++ codebert_analysis.py
@@ -0,0 +1,8 @@
+# codebert_analysis.py
+
+print("📊 Code Security Report (Simulasi)\n")
+print("✅ Status: Kerentanan ditemukan.")
+print("Tingkat: Sedang")
+print("Jenis: Hardcoded Credential")
+print("File: test.txt")
+print("Baris: 12")
